1e1d333079718e1508c1d2de10b99222e50cdb44
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:18:26 +0200

    release: v18.0.21

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'isomorphic-region-loader.example.domain.com',
+    title: 'Isomorphic Region Loader',
+  },
+};
+export default env;
