4c5dc836a5f501479bf3253d176445c2eb49f8d7
Dariusz Filipiak <darekf77@gmail.com>
2025-04-22 16:21:56 +0200

    chore: update

--- src/app.ts
+++ src/app.ts
@@ -34,4 +34,21 @@
 /* */
 /* */
 /* */
-  
+
+//#region  isomorphic-region-loader component
+//#region @browser
+@Component({ template: 'hello world fromr isomorphic-region-loader' })
+export class IsomorphicRegionLoaderComponent {}
+//#endregion
+//#endregion
+
+//#region  isomorphic-region-loader module
+//#region @browser
+@NgModule({
+  declarations: [IsomorphicRegionLoaderComponent],
+  imports: [CommonModule],
+  exports: [IsomorphicRegionLoaderComponent],
+})
+export class IsomorphicRegionLoaderModule {}
+//#endregion
+//#endregion
