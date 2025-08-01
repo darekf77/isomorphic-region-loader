db6d7d23359dde897cdc47018fa184e3ced920a1
Dariusz Filipiak <darekf77@gmail.com>
2025-08-01 10:49:28 +0200

    release: v19.0.35

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -2,6 +2,6 @@
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.isomorphic-region-loader';
 export const PROJECT_NPM_NAME = 'isomorphic-region-loader';
-export const CURRENT_PACKAGE_VERSION = '19.0.34';
+export const CURRENT_PACKAGE_VERSION = '19.0.35';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
